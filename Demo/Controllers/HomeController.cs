ce6a201bcc4c0fb7e4fb58777b78e62427a3bcb4
felicitycunningham <felicity.cunningham@williamhill.com.au>
2014-09-08 15:02:42 +1000

    Adding files

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Demo.Controllers {
+    using Twilio.TwiML;
+    using Twilio.TwiML.Mvc;
+
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public virtual TwiMLResult CalledLive(string bets, string callerName) {
+            var call = new TwilioResponse();
+            call.Say("Hello");
+            call.Say(bets);
+            call.Say("Connecting now");
+            ////call.Dial(new Client(callerName));
+            return new TwiMLResult(call.ToString());
+        }
+    }
+
+}
